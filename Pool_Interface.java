81fec3debc1b77b428089e265ef7c040f45f79ff
OraMk <163622868+OraMk@users.noreply.github.com>
2024-03-16 15:52:51 +0800

    Delete Pool_Interface.java

--- Pool_Interface.java
+++
@@ -1,9 +0,0 @@
-package The_First_week.HandWriting_JDBC;
-
-import java.sql.Connection;
-
-public interface Pool_Interface {
-    Connection getconnection();//获取连接
-    void ReleaseConnection (Connection connection);//释放连接，使得connection 空闲可以被调用
-    void shutdown();
-}
